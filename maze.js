c535aa2ab14262a95677a0473a062fc9a190d8b0
maurice murray <maurice murray>
2016-10-14 03:48:53 -0500

    Commit solution for exercise 2

--- maze.js
+++ maze.js
@@ -1,10 +1,26 @@
 
-window.onload = function()
+window.onload = function() // Actions to do after window load
 {
-
-		var change = document.getElementById("boundary1");		
-		change.onmouseover = function()
-		{
-			change.className= "boundary youlose" ;
-		}	
+        var boundaries= document.querySelectorAll(".boundary");	// Selection of the boundaries		
+		
+	
+			for( var x = 0; x < boundaries.length-1; x++)   // Addition of multiple event listeners
+			{
+				boundaries[x].addEventListener("mouseover", colorChange() );
+		    }
+			
+			
+			
+			function colorChange() // Function handling event
+			{   
+			  for( var i= 0; i < boundaries.length-1; i++) // Loop checking for mouse hover event 
+			    boundaries[i].onmouseover= function()
+			    {  
+				  for( var i= 0; i < boundaries.length-1; i++) // Loop to select all the boundaries
+					boundaries[i].className+=" youlose";
+				}			
+					
+			}
+		
 }
+
