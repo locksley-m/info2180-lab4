faaa31534cf7b1ec5ec9697cfdd06d25017ac02c
maurice murray <maurice murray>
2016-10-14 01:33:01 -0500

    Initial commit for part 1

---
+++ maze.js
@@ -0,0 +1,10 @@
+
+window.onload = function()
+{
+
+		var change = document.getElementById("boundary1");		
+		change.onmouseover = function()
+		{
+			change.className= "boundary youlose" ;
+		}	
+}
