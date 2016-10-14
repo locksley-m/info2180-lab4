5269e0a11a8e984b56092ddce19983cc0507501a
maurice murray <maurice murray>
2016-10-14 04:33:32 -0500

    Commit solution for Exercise 2

--- maze.js
+++ maze.js
@@ -2,13 +2,8 @@
 window.onload = function() // Actions to do after window load
 {
     var boundaries= document.querySelectorAll(".boundary");	// Selection of the boundaries		
-	var start = document.getElementById("start");	
-	var end = document.getElementById("end");
-	var started = false; // Initialization
-	var bhit= false;
-	var finished =false;
+		
 	
-	    start.addEventListener("mouseover", function(){ started = true;});
 		for( var x = 0; x < boundaries.length-1; x++)   // Addition of multiple event listeners
 		    {
 				boundaries[x].addEventListener("mouseover", colorChange() );
@@ -23,20 +18,9 @@ window.onload = function() // Actions to do after window load
 			    {  
 				  for( var i= 0; i < boundaries.length-1; i++) // Loop to select all the boundaries
 					boundaries[i].className+=" youlose";
-					bhit= true;// Set if hit 
 				}			
-			  if(bhit){		
-			  alert("Oh no! Try again..");}
+					
 			}
-	end.addEventListener("mouseover", function()
-	{  
-	   finished = true;
-	   if(start && finished&& (!bhit)) // Event handler to alert player 
-		   alert("You Win !");
-	    
-		   
-	    
-	   
-	});	
+		
 }
 
